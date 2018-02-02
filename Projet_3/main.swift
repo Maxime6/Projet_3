010aa0d3a3406e4c94f2c360261d2874466a8066
Maxime6 <maxime6@github.com>
2018-02-02 10:10:08 +0100

    Project initialization

---
+++ Projet_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet_3
+//
+//  Created by Maxime Tanter on 02/02/2018.
+//  Copyright © 2018 Maxime Tanter. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
