c7ff0824556689b0a7e2d56ac2a4b9a24ecf2c7f
Maxime6 <maxime6@github.com>
2018-02-15 17:48:53 +0100

    Editing Team class, with loop for in the function createCharacter

--- Projet_3/Game/Team.swift
+++ Projet_3/Game/Team.swift
@@ -16,40 +16,68 @@ class Team {
     
     func createCharacter() {
         
-        for i in 1..<4 {
-            print("Choose the name of your \(i) character :")
+        for i in 1...3 {
             
+            print("Choose the name of your character \(i) :")
             if let name = readLine() {
                 names.append(name)
-                print("\(name)")
             }
             
-        }
-        
-        for _ in 0..<3 {
-            
-            print("Select the class of your character :")
+            print("Select the class of your character \(i)")
             print("1. Fighter")
             print("2. Magus")
             print("3. Colossus")
             print("4. Dwarf")
-            
             if let choice = readLine() {
                 switch choice {
                 case "1": // Create a Fighter
-                    characters.append(Fighter(name: ""))
+                    characters.append(Fighter(name: names[i]))
                 case "2": // Create a Magus
-                    characters.append(Magus(name: ""))
+                    characters.append(Magus(name: names[i]))
                 case "3": // Create a Colossus
-                    characters.append(Colossus(name: ""))
+                    characters.append(Colossus(name: names[i]))
                 case "4": // Create a Dwarf
-                    characters.append(Dwarf(name: ""))
+                    characters.append(Dwarf(name: names[i]))
                 default: break
                 }
             }
         }
-        // cette fonction doit remplir le tableau characters avec 3 personnages
         
+//        for i in 1..<4 {
+//            print("Choose the name of your character \(i) :")
+//
+//            if let name = readLine() {
+//                names.append(name)
+//            }
+//
+//
+//        }
+//
+//        for _ in 1..<4 {
+//            print("Select the class of your character :")
+//            print("1. Fighter")
+//            print("2. Magus")
+//            print("3. Colossus")
+//            print("4. Dwarf")
+//            // Choisir un numéro entre 1 et 4, 3 fois
+//
+//        }
+//
+//
+//
+//        if let choice = readLine() {
+//            switch choice {
+//            case "1": // Create a Fighter
+//            characters.append(Fighter(name: ""))
+//            case "2": // Create a Magus
+//            characters.append(Magus(name: ""))
+//            case "3": // Create a Colossus
+//            characters.append(Colossus(name: ""))
+//            case "4": // Create a Dwarf
+//            characters.append(Dwarf(name: ""))
+//            default: break
+//          }
+//        }
 
     }
     
